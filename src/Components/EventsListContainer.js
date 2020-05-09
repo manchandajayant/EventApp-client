4dc0d770fcb18b4ad51d9e1d8d78a200ba95dc0a
manchandajayant <maanjayant@gmail.com>
2020-05-09 15:27:04 +0200

    card media addedd

--- src/Components/EventsListContainer.js
+++ src/Components/EventsListContainer.js
@@ -2,9 +2,53 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { showAllEvents } from "../Actions/eventActions";
 import AddNewEventContainer from "./AddNewEventContainer";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 const EventListContainer = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const events = useSelector((state) => state.events);
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   useEffect(() => {
     dispatch(showAllEvents());
   }, []);
@@ -13,9 +57,55 @@ const EventListContainer = () => {
     <div>
       {events.map((event, index) => (
         <div key={index}>
-          <h1>{event.name}</h1>
-          <p>{event.description}</p>
-          <img src={event.url} alt="No" />
+          <Card className={classes.root}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  R
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={event.name}
+              subheader={event.startDate}
+            />
+            <CardMedia
+              className={classes.media}
+              image={event.url}
+              title={event.name}
+            />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {event.description}
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+              <IconButton
+                className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>{event.description}</Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
         </div>
       ))}{" "}
       <AddNewEventContainer />
